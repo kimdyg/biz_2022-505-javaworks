3416f9766bf0dce0f40e56a5bc4f6e13d176fbdc
kimdyg <rlaeldud9502@naver.com>
2022-03-22 09:19:31 +0900

    hello

---
+++ HelloKorea.java
@@ -0,0 +1,12 @@
+/*
+ Multi line This is comment
+*/
+// single line comment
+
+public class HelloKorea {
+      //comment
+	public static void main(String[] args) {
+		System.out.println("Hello Korea");
+
+	}
+}
